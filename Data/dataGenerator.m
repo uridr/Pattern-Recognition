c39e6062af327742f9f04ddf734dd6605a515531
uridr <orioldomingoroig@gmail.com>
2019-04-12 18:44:36 +0200

    TrSeed & TeSeed

--- Data/dataGenerator.m
+++ Data/dataGenerator.m
@@ -11,9 +11,10 @@ for j=1:ncol
     
     if rand() < (freq-0.1*nT)/(1-0.1*nT) i = target(randi([1,nT]));
     else i = randi(10); end
-    if i == 0 X(:,j) = N(:,end);
-    else X(:,j) = N(:,i);
-    end
+    
+    if i == 0  X(:,j) = N(:,end);
+    else       X(:,j) = N(:,i); end
+    
     if any(i == target) y(j) = 1;
     else y(j) = 0; end
     
--- Data/dataSets.m
+++ Data/dataSets.m
@@ -1,6 +1,6 @@
-function [Xtr, ytr, Xte, yte] = dataSets(seed, ncol, target, freq, noise_freq)
+function [Xtr, ytr, Xte, yte] = dataSets(trseed, teseed, ncol, target, freq, noise_freq)
 
-[Xtr, ytr] = dataGenerator(seed, ncol, target, freq, noise_freq);
-[Xte, yte] = dataGenerator(seed, 10*ncol, target, 0, noise_freq);
+[Xtr, ytr] = dataGenerator(trseed, ncol, target, freq, noise_freq);
+[Xte, yte] = dataGenerator(teseed, 10*ncol, target, 0, noise_freq);
 
 end
