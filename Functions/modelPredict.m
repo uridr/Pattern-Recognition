cf6bdb05dd9825914af0f213e85a804755c03e41
uridr <orioldomingoroig@gmail.com>
2019-04-28 17:50:18 +0200

    Sigmoide

--- Functions/modelPredict.m
+++ Functions/modelPredict.m
@@ -3,11 +3,11 @@ function[indicator] = modelPredict(model, object)
 sig = @(X)   1./(1+exp(-X)); 
 y   = @(X,w) sig(w'*sig(X));
 
-predict = round(y(object,model)); %Round or Sigmoide (ToDecide)
+predict = round(y(object,model)); 
 
 %If model recognizes obj as one in chosen subset indicator = 1
 
-if predict >= 1 indicator = 1;
+if predict == 1 indicator = 1;
 else            indicator = 0; end
 
 end
